dc1e6d2e8c75e6f227bc76f24cdd9b8f0f180eda
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-09 14:04:54 +0800

    Finish up example by hws+

--- Swift 5.3/ContentView.swift
+++ Swift 5.3/ContentView.swift
@@ -8,21 +8,65 @@
 import MapKit
 import SwiftUI
 
-struct ContentView: View {
-    @State private var tapCount = 0
+struct SampleRow: View {
+    let id: Int
     
     var body: some View {
-        Button {
-            tapCount += 1
-        } label: {
-            Image(systemName: "star")
-            Text("Tap count \(tapCount)")
-        }
+        Text("Row \(id)")
+    }
+    
+    init(id: Int) {
+        self.id = id
+        print("Loading row \(id)")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct ContentView: View {
+    //    let countries = Bundle.main.decode([String].self, from: "countries.json")
+        @State private var position = 0
+    
+    var body: some View {
+        VStack {
+            Text(String(position)).font(.largeTitle)
+            
+            ScrollView {
+                ForEach(0..<100) { i in
+                    LazyVStack {
+                        Text(String(i))
+                            .onAppear {
+                                position = i
+                            }
+                    }
+                }
+            }
+        }
+      
+        // MARK: - Understand lazy creation view
+        
+        //        ScrollView {
+        //            LazyVStack {
+        //                ForEach(1...1000, id: \.self, content: SampleRow.init)
+        //    }
+        //}
+        
+        
+        // MARK: - Show flags
+        //            LazyVStack(alignment: .leading) {
+        //                ForEach(countries, id: \.self) { country in
+        //                    Image(country)
+        //                        .resizable()
+        //                        .frame(width: 120, height: 60)
+        //                        .border(Color.black, width: 1)
+        //
+        //                    Text(country)
+        //                        .font(.title)
+        //                }
+        //    }
     }
 }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
