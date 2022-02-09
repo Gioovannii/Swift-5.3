32b89c659259f1c62e055f9c49be0c428349f636
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-09 10:00:29 +0800

    Initial Commit

---
+++ Swift 5.3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift 5.3
+//
+//  Created by Giovanni Gaffé on 2022/2/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift 5.3/Swift_5_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_5_3App.swift
+//  Swift 5.3
+//
+//  Created by Giovanni Gaffé on 2022/2/9.
+//
+
+import SwiftUI
+
+@main
+struct Swift_5_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
