fc24ad780f0dc3391cece219304ea32648b0e7d0
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-09 10:14:05 +0800

    remove all self

--- Swift 5.3/ContentView.swift
+++ Swift 5.3/ContentView.swift
@@ -5,12 +5,16 @@
 //  Created by Giovanni Gaffé on 2022/2/9.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap count \(tapCount)") {
+            tapCount += 1
+        }
     }
 }
 
